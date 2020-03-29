7cd10b1fe6678c9bc4800adcc898182784805e79
kevmodrome <kevin@kevmodro.me>
2020-03-29 13:06:46 +0200

    initial commit

---
+++ src/pages/_layout.svelte
@@ -0,0 +1,58 @@
+<style>
+  .shaded {
+    width: 100%;
+    background: #f3f6f9;
+  }
+  header {
+    padding: 64px 1rem;
+  }
+  nav {
+    margin: 0;
+  }
+  h1 {
+    margin: 0 0 0 35px;
+    padding: 0;
+    text-align: center;
+  }
+  a {
+    color: black;
+    font-size: 19px;
+  }
+  li a:hover {
+    text-decoration: none;
+    border-bottom: 4px solid var(--color);
+  }
+  .logo {
+    display: flex;
+    align-items: center;
+  }
+</style>
+
+<svelte:head>
+  <title>Svelte Society Day</title>
+</svelte:head>
+
+<div class="shaded">
+  <header>
+    <nav>
+      <div class="logo">
+        <a href="/">
+          <img alt="Svelte Society Logo" src="./logo.jpg" />
+        </a>
+        <h1>SVELTE SOCIETY DAY</h1>
+      </div>
+      <ul>
+        <li>
+          <a href="#docs">HOME</a>
+        </li>
+        <li>
+          <a href="#docs">DOCS</a>
+        </li>
+        <li>
+          <a href="#faq">FAQ</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</div>
+<slot />
---
+++ src/pages/index.svelte
@@ -0,0 +1,11 @@
+<script>
+  import Introduction from "components/Introduction.svelte";
+  import CallForProposals from "components/CallForProposals.svelte";
+  import FAQ from "components/FAQ.svelte";
+</script>
+
+<main>
+  <Introduction />
+  <CallForProposals />
+</main>
+<FAQ />
